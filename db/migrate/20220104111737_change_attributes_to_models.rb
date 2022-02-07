3b62168553ac79b2296816b2812777e29dd0dd50
Hugo Daniel <hugodaniel.icam@gmail.com>
2022-02-07 08:17:27 +0100

    WIP

--- db/migrate/20220104111737_change_attributes_to_models.rb
+++ db/migrate/20220104111737_change_attributes_to_models.rb
@@ -1,7 +1,7 @@
 class ChangeAttributesToModels < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :user_projects, :user
-    remove_reference :user_projects, :project
+    # remove_reference :user_projects, :user
+    # remove_reference :user_projects, :project
     add_reference :projects, :user
     add_reference :projects, :employer
   end
